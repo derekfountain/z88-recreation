7aa0031bf657c7c44ef5dd9163155adec3239f4e
Derek Fountain <derek@scratters.com>
2026-01-09 15:04:55 +0000

    application board blink example

---
+++ software/application-board/app-board-blink/app_board_blink.c
@@ -0,0 +1,80 @@
+/*
+ * Pico application board example
+ * Copyright (C) 2026 Derek Fountain
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * cmake -DCMAKE_BUILD_TYPE=Debug ..
+ * make -j10
+ *
+ * With the RPi Debug Probe:
+ * 
+ * sudo openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" \
+ *              -c "program app_board_blink.elf verify reset exit"
+ * sudo openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000"
+ *
+ * gdb-multiarch app_board_blink.elf
+ *  target remote localhost:3333
+ *  load
+ *  monitor reset init
+ *  continue
+ *
+ * With the home made Pico probe:
+ *
+ * sudo openocd -f interface/picoprobe.cfg -f target/rp2040.cfg \
+ *              -c "program ./app_board_blink.elf verify reset exit"
+ *
+ * On the z88r:
+ *
+ * scp app_board_blink.elf z88r:tmp
+ *
+ * sudo pinctrl set 23 op
+ * sudo pinctrl set 24 op
+ * sudo pinctrl set 23 dh; sudo pinctrl set 24 dl
+ * sudo openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program ./app_board_blink.elf verify reset exit"
+ *
+ */
+
+#include <stdio.h>
+#include "pico.h"
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+
+const uint LED_PIN              = PICO_DEFAULT_LED_PIN;
+
+int main()
+{
+  bi_decl(bi_program_description("Pico application board blink example binary."));
+
+  gpio_init( LED_PIN );
+  gpio_set_dir( LED_PIN, GPIO_OUT );
+
+  while(1)
+  {
+    gpio_put(LED_PIN, 1);
+
+    sleep_ms(1000);
+
+    gpio_put(LED_PIN, 0);
+
+    sleep_ms(1000);
+  }
+}
